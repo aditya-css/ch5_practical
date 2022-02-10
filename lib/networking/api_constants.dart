41012fbe8eaf3f55a7073f94e59d7d5f138d7aac
aditya-css <aditya.c@simformsolutions.com>
2022-02-10 10:47:33 +0530

    retrofit class setup done

---
+++ lib/networking/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConst {
+  //base
+  String get apiKey => '5377d5133c384339aa839c6bcdfe9d6f';
+  String get baseUrl => 'https://newsapi.org/v2';
+
+  //endpoints
+  static const String indiaHeadlines = '/top-headlines?country=in';
+}
---
+++ lib/networking/models/article_model.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'article_model.g.dart';
+
+@JsonSerializable()
+class Article {
+  late final Map<String, dynamic> source;
+  late final String? author, content;
+  late final String title, description;
+  late final String url, urlToImage, publishedAt;
+
+  Article({
+    required this.source,
+    this.author,
+    this.content,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) =>
+      _$ArticleFromJson(json);
+}
