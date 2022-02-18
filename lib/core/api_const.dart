c9194bc0c369c4e5787e8ad502384b256d95d210
aditya-css <aditya.c@simformsolutions.com>
2022-02-18 21:58:54 +0530

    add internet permission

--- lib/core/api_const.dart
+++ lib/core/api_const.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 class ApiConst {
   //base
   static const String kApiKey = '5377d5133c384339aa839c6bcdfe9d6f';
+  // static const String kApiKey = '06e4a1e179734ea28b66bcf21de6eea7';
   static const String kBaseUrl = 'https://newsapi.org/v2/';
 
   //endpoints
